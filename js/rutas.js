a0fa99b8f58676acbead86b0b4da2db23787ceff
sfggeogis <sfggeogis@gmail.com>
2021-01-25 20:15:54 +0100

    add comparador

--- js/rutas.js
+++ js/rutas.js
@@ -32,3 +32,20 @@ function zoomToRutas(valores) {
         });
 
     } //fin funcion
+
+    var animacion;
+    function rotarCamara(timestamp) {
+
+        //console.info("timestamp",timestamp);
+        rotacion =timestamp /100 ==360 ?0 : timestamp /100;
+        //console.info("rotacion",rotacion);
+        map.rotateTo(rotacion, { duration: 0 });
+
+        animacion = requestAnimationFrame(rotarCamara);
+    }
+
+
+    function finalRotarCamara() {
+
+        cancelAnimationFrame(animacion);
+    }   
